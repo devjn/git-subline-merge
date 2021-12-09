1ee62f99ce401535e06e96dc5fdd51f8a461c72e
Alisher Matkurbanov <alishermatkurbanov@gmail.com>
2021-12-09 15:15:29 +0500

    add safety

--- runner.py
+++ runner.py
@@ -7,29 +7,17 @@ if __name__ == "__main__":
     parser.add_argument(
         "--filenames", type=str, nargs="+", help="filenames for another scripts"
     )
-    parser.add_argument(
-        "--python",
-        type=str,
-        default="python",
-        help="python version to run the merge script with",
-    )
-    parser.add_argument(
-        "--merge-script",
-        type=str,
-        default="git-subline-merge",
-        help="merge script path",
-    )
     args = parser.parse_args()
 
-    if not os.path.exists(args.merge_script):
-        print(f"Can't find merge script {args.merge_script}")
+    if not os.path.exists("git-subline-merge"):
+        print(f"Can't find merge script git-subline-merge")
         exit(-1)
 
     print("Running merge script on files...")
     for filename in args.filenames:
         print(f"handling {filename}: ", end="")
         proc = subprocess.run(
-            [args.python, args.merge_script, filename], capture_output=True
+            ["python", "git-subline-merge", filename], capture_output=True
         )
         if proc.returncode == 0:
             print("OK")
